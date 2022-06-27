1752685fdea0aed461e095fb26a8ee0009f49d61
amruta kondalkar <kondalkaramruta06@gmail.com>
2022-06-27 10:38:31 +0530

    committing the changes made in LoginTest

--- src/main/java/com/sample/NewMavenProject/LoginTest.java
+++ src/main/java/com/sample/NewMavenProject/LoginTest.java
@@ -18,6 +18,7 @@ public class LoginTest {
 	{
 		System.setProperty("webdriver.chrome.driver", "C:\\Selenium_Driver\\chromedriver_102.exe");
 		driver = new ChromeDriver();
+		System.out.println("Launching browser");
 		
 		//driver.get("https://login.salesforce.com/?locale=in");
 	}
@@ -25,11 +26,18 @@ public class LoginTest {
 	public void doLogin()
 	{
 		System.out.println("Executing Login Test");
+		System.out.println("Login to facebook account");
+	
 		driver.get("https://www.facebook.com/");
 		driver.manage().timeouts().implicitlyWait(10L,TimeUnit.SECONDS);
 		
+		System.out.println("Entering username");
 		driver.findElement(By.id("email")).sendKeys("kondalkaramruta06@gmail.com");
+		
+		System.out.println("Entering password");
 		driver.findElement(By.id("pass")).sendKeys("Amru#09");
+		
+		System.out.println("Clicking on login button ");
 		driver.findElement(By.name("login")).click();
 		
 		System.out.println("Logging into facebook account");
